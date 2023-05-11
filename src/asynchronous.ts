00f70713654a5c796166f4f4d9bd41175b8b43da
Md Imran Hossen <freelancerimran81@gmail.com>
2023-05-11 10:33:10 +0600

    Done

--- src/asynchronous.ts
+++ src/asynchronous.ts
@@ -1,6 +1,17 @@
 // Mocking 
-const makePromise = () =>{
-    return new Promise((resolve, reject) => {
+// const makePromise = () =>{
+//     return new Promise((resolve, reject) => {
+//         const data : string = 'Data is fetched from server';
+//         if(data){
+//             resolve(data);
+//         }else{
+//             reject('No data found');
+//         }
+//     }
+//     )
+// }
+const makePromise = ():Promise<string> =>{
+    return new Promise<string>((resolve, reject) => {
         const data : string = 'Data is fetched from server';
         if(data){
             resolve(data);
@@ -10,3 +21,12 @@ const makePromise = () =>{
     }
     )
 }
+
+
+const getPromiseData = async ():Promise<string> =>{
+    const data = await makePromise();
+    return data;
+}
+
+// Promise <string> Promise <number> Promise <boolean> Promise <object>
+
