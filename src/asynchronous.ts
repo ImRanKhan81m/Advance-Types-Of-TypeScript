b4d59987a09ad18901b048a509079cc18ac089ae
Md Imran Hossen <freelancerimran81@gmail.com>
2023-05-12 00:54:00 +0600

    done

--- src/asynchronous.ts
+++ src/asynchronous.ts
@@ -21,12 +21,43 @@ const makePromise = ():Promise<string> =>{
     }
     )
 }
-
-
 const getPromiseData = async ():Promise<string> =>{
     const data = await makePromise();
     return data;
 }
 
-// Promise <string> Promise <number> Promise <boolean> Promise <object>
+const makePromiseBoolean = ():Promise<boolean> =>{
+    return new Promise<boolean>((resolve, reject) => {
+        const data : boolean = false;
+        if(data){
+            resolve(data);
+        }else{
+            reject('No data found');
+        }
+    }
+    )
+}
+
+const getPromiseDataBoolean = async ():Promise<boolean> =>{
+    const data = await makePromiseBoolean();
+    return data;
+}
+
+const makePromiseObject = ():Promise<object> =>{
+    return new Promise<object>((resolve, reject) => {
+        const data : object = {name: 'John', age: 30};
+        if(data){
+            resolve(data);
+        }else{
+            reject('No data found');
+        }
+    }
+    )
+}
+
+const getPromiseDataObject = async ():Promise<object> =>{
+    const data = await makePromiseObject();
+    return data;
+}
+
 
